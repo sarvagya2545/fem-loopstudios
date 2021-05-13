28b5c7c13c8ceb33cc94d9e2c6065a4fc88b9249
Sarvagya Sharma <sarvagya2545@gmail.com>
2021-05-13 10:00:54 +0530

    add navbar animation

--- js/animateonscroll.js
+++ js/animateonscroll.js
@@ -1,11 +1,18 @@
 const fadingObjects = document.querySelectorAll('.fade');
+const navbar = document.querySelector('.nav');
+const hero = document.querySelector('.hero');
 
 const appearOnScrollOptions = {
     threshold: 0.2,
     rootMargin: "0px 0px -150px 0px"
 };
 
+const navBarOptions = {
+    threshold: 0.8,
+};
+
 const appearOnScrollObserver = new IntersectionObserver(appearOnScroll, appearOnScrollOptions);
+const navBarObserver = new IntersectionObserver(changeBgOnScroll, navBarOptions);
 
 function appearOnScroll(entries, observer) {
     entries.forEach(entry => {
@@ -17,4 +24,17 @@ function appearOnScroll(entries, observer) {
     })
 }
 
+function changeBgOnScroll(entries, observer) {
+    entries.forEach(entry => {
+        // console.log(entry);
+        // console.log(entry.target);
+        if(!entry.isIntersecting) {
+            navbar.classList.add('navbar-scrolled')
+        } else {
+            navbar.classList.remove('navbar-scrolled')
+        }
+    })
+}
+
 fadingObjects.forEach(fadingObject => appearOnScrollObserver.observe(fadingObject));
+navBarObserver.observe(hero);
